a23634f30d706479153c88411165e668a737b503
larama-C <ccbj0825@naver.com>
2025-11-28 12:05:57 +0900

    fixed kakao login

--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -50,7 +50,9 @@ export default function ClientLayout({
           <Link href="/posts">글 목록</Link>
           {!isLogin && <Link href="/members/login">로그인</Link>}
           {!isLogin && (
-            <Link href="http://localhost:8080/oauth2/authorization/kakao">
+            <Link
+              href={`${process.env.NEXT_PUBLIC_API_BASE_URL}/oauth2/authorization/kakao?redirectUrl=${process.env.NEXT_PUBLIC_FRONT_BASE_URL}`}
+            >
               카카오 로그인
             </Link>
           )}
